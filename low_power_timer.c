077b8da21c785e6d98ed7d1590c48e079fd4f250
Alessio <48206283+ooiisssseellaa@users.noreply.github.com>
2019-03-04 12:41:52 +0100

    Add files via upload

---
+++ low_power_timer.c
@@ -0,0 +1,36 @@
+/*
+ * low_power_timer.c
+ *
+ *  Created on: 01 mar 2019
+ *      Author: Alessio
+ */
+#include "low_power_timer.h"
+
+void LPTMR0_init(void)
+{
+  // LPTMR
+    PORTB->PCR[21] |= (1 << 8);	// red led enable
+    GPIOB->PDDR |= (1 << 21);
+    GPIOB->PSOR |= (1 << 21);
+
+    NVIC->IP[58] = (1 << 6); // Preemptive Priority
+
+    SIM->SCGC5 |= (1 << 0);
+
+    LPTMR0->PSR = (1 << 2) | (1 << 0);
+    LPTMR0->CMR = 90000000;
+    LPTMR0->CSR = (1 << 0) | (1 << 6); // Timer enable and interrupt enable
+
+    NVIC->ISER[1] |= (1 << 26) ;
+}
+
+void LPTMR0_IRQHandler(void)
+{
+	GPIOB->PCOR |= (1 << 21); // Blue Led ON
+
+	delay (1000);
+
+	GPIOB->PSOR |= (1 << 21); // Blue Led OFF
+
+	LPTMR0->CSR |= (1 << 7);
+}
